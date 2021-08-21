7118a2412fc08df9d4e5d52dc8d4a7eef26a0d0a
wilhelm <helm@hey.com>
2021-08-21 17:09:54 +1000

    Implement Basic Weather Update

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/wilhelmeek/dayforit
 
 go 1.16
+
+require (
+	github.com/briandowns/openweathermap v0.16.0
+	github.com/gorilla/mux v1.8.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1
+	github.com/slack-go/slack v0.9.4
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,168 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/slack-go/slack"
+
+	owm "github.com/briandowns/openweathermap"
+)
 
 func main() {
-	fmt.Println("launched")
+	logger := logrus.New()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/check", handleCheck)
+
+	port := "8080"
+	if p := os.Getenv("PORT"); p != "" {
+		port = p
+	}
+
+	err := http.ListenAndServe(":"+port, r)
+	if err != nil {
+		logger.WithError(err).Info("handling http traffic")
+	}
+}
+
+func handleCheck(w http.ResponseWriter, r *http.Request) {
+	wtr, err := owm.NewCurrent(
+		"C",
+		"EN",
+		os.Getenv("OWM_KEY"),
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	wtr.CurrentByZip(2017, "AU")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	windSpeedKPH := int(math.Trunc(wtr.Wind.Speed * 60 * 60 / 1000))
+	windDirection := directionFromDegrees(wtr.Wind.Deg)
+	primaryWeather := wtr.Weather[0]
+
+	var title, subtitle string
+	switch true {
+	case primaryWeather.ID == 800:
+		title = "It is an absolute day for it! :sunny:"
+		subtitle = "The weather today is *perfect*. Good day to get on the bikes!"
+	case primaryWeather.ID > 800:
+		title = "No reason not to cycle."
+		subtitle = "The weather today is perfectly cycleable. Get on the bikes!"
+	case primaryWeather.ID >= 300 && primaryWeather.ID < 400:
+		title = "Worth a shot!"
+		subtitle = "It's a bit drizzly today, but gotta risk it for the biscuit."
+	default:
+		title = "Nope :no_good:"
+		subtitle = "Not today."
+	}
+
+	err = slack.PostWebhook(
+		os.Getenv("SLACK_WEBHOOK_URL"),
+		&slack.WebhookMessage{
+			Text: title,
+			Blocks: &slack.Blocks{
+				BlockSet: []slack.Block{
+					slack.NewHeaderBlock(
+						slack.NewTextBlockObject(
+							slack.PlainTextType,
+							title,
+							false,
+							false,
+						),
+					),
+					slack.NewSectionBlock(
+						slack.NewTextBlockObject(
+							slack.MarkdownType,
+							subtitle,
+							false,
+							false,
+						),
+						nil,
+						nil,
+					),
+					slack.NewDividerBlock(),
+					slack.NewSectionBlock(
+						nil,
+						[]*slack.TextBlockObject{
+							slack.NewTextBlockObject(
+								slack.MarkdownType,
+								fmt.Sprintf("*Currently*: %v°", wtr.Main.Temp),
+								false,
+								false,
+							),
+							slack.NewTextBlockObject(
+								slack.MarkdownType,
+								fmt.Sprintf(
+									"*Range*: %v° - %v°",
+									wtr.Main.TempMin,
+									wtr.Main.TempMax,
+								),
+								false,
+								false,
+							),
+							slack.NewTextBlockObject(
+								slack.MarkdownType,
+								fmt.Sprintf("*Rain*: %vmm", wtr.Rain.ThreeH),
+								false,
+								false,
+							),
+							slack.NewTextBlockObject(
+								slack.MarkdownType,
+								fmt.Sprintf(
+									"*Windspeed*: %vkm/h %v",
+									windSpeedKPH,
+									windDirection,
+								),
+								false,
+								false,
+							),
+						},
+						nil,
+					),
+				},
+			},
+		},
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
+
+func directionFromDegrees(deg float64) string {
+	idx := int(math.Round(math.Mod(deg, 360) / 22.5))
+
+	return []string{
+		"N",
+		"NNE",
+		"NE",
+		"ENE",
+		"E",
+		"ESE",
+		"SE",
+		"SSE",
+		"S",
+		"SSW",
+		"SW",
+		"WSW",
+		"W",
+		"WNW",
+		"NW",
+		"NNW",
+	}[idx]
 }
